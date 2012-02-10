d0ab68f960d1de1d77ff05d9189a78a8ba6981a3
brownson <brownson@gmx.at>
2012-02-10 15:42:03 +0100

    Some Fixes for Download from GIT

--- IPSLibrary/install/BaseLoader/IPSLibrary_BaseLoader.ips.php
+++ IPSLibrary/install/BaseLoader/IPSLibrary_BaseLoader.ips.php
@@ -47,7 +47,7 @@
 			curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
 			echo 'Load File '.$sourceFile."\n";
 			$fileContent = curl_exec($curl_handle);
-			if (strpos($fileContent, 'Something went wrong with that request. Please try again') > 0) {
+			if (strpos($fileContent, 'Something went wrong with that request. Please try') > 0) {
 			   die('File '.$sourceFile.' could NOT be found on the Server !!!'.PHP_EOL);
 			}
 			curl_close($curl_handle);
