262d6c49285f4e4abc68a276e5f101194f96eecd
brownson <brownson@gmx.at>
2012-04-18 19:30:52 +0200

    IPSComponent - Added IPSComponentSwitch_LCN

---
+++ IPSLibrary/app/core/IPSComponent/IPSComponentSwitch/IPSComponentSwitch_LCN.class.php
@@ -0,0 +1,77 @@
+<?
+	/**@addtogroup ipscomponent
+	 * @{
+	 *
+ 	 *
+	 * @file          IPSComponentSwitch_LCN.class.php
+	 * @author        Andreas Brauneis
+	 *
+	 *
+	 */
+
+   /**
+    * @class IPSComponentSwitch_LCN
+    *
+    * Definiert ein IPSComponentSwitch_LCN Object, das ein IPSComponentSwitch Object für LCN implementiert.
+    *
+    * @author Andreas Brauneis
+    * @version
+    * Version 2.50.1, 31.01.2012<br/>
+    */
+
+	IPSUtils_Include ('IPSComponentSwitch.class.php', 'IPSLibrary::app::core::IPSComponent::IPSComponentSwitch');
+
+	class IPSComponentSwitch_LCN extends IPSComponentSwitch {
+
+		private $instanceId;
+	
+		/**
+		 * @public
+		 *
+		 * Initialisierung eines IPSComponentSwitch_LCN Objektes
+		 *
+		 * @param integer $instanceId InstanceId des LCN Devices
+		 */
+		public function __construct($instanceId) {
+			$this->instanceId = IPSUtil_ObjectIDByPath($instanceId);
+		}
+
+		/**
+		 * @public
+		 *
+		 * Function um Events zu behandeln, diese Funktion wird vom IPSMessageHandler aufgerufen, um ein aufgetretenes Event 
+		 * an das entsprechende Module zu leiten.
+		 *
+		 * @param integer $variable ID der auslösenden Variable
+		 * @param string $value Wert der Variable
+		 * @param IPSModuleSwitch $module Module Object an das das aufgetretene Event weitergeleitet werden soll
+		 */
+		public function HandleEvent($variable, $value, IPSModuleSwitch $module){
+		}
+
+		/**
+		 * @public
+		 *
+		 * Zustand Setzen 
+		 *
+		 * @param boolean $value Wert für Schalter
+		 */
+		public function SetState($value) {
+			LCN_SwitchRelay($this->instanceId, true);
+		}
+
+		/**
+		 * @public
+		 *
+		 * Liefert aktuellen Zustand
+		 *
+		 * @return boolean aktueller Schaltzustand  
+		 */
+		public function GetState() {
+			GetValue(IPS_GetVariableIDByName('Status', $this->instanceId));
+		}
+
+	}
+
+	/** @}*/
+?>
