e6a0e782d8fd18284c087fa32bd2de989ec2891c
brownson <brownson@gmx.at>
2012-05-07 20:46:33 +0200

    IPSWatering - Fixed Logging Message

--- IPSLibrary/install/InstallationScripts/IPSWatering_Installation.ips.php
+++ IPSLibrary/install/InstallationScripts/IPSWatering_Installation.ips.php
@@ -255,32 +255,6 @@
 		}
 	}
 
-	RemoveBlanksBeforePHPTags('IPSWatering_Configuration.inc.php', 'IPSLibrary::config::modules::IPSWatering::Default');
-	RemoveBlanksBeforePHPTags('IPSWatering_Configuration.inc.php', 'IPSLibrary::config::modules::IPSWatering');
-	RemoveBlanksBeforePHPTags('IPSUtils.inc.php',                  'IPSLibrary::app::core::IPSUtils');
-
-	// ------------------------------------------------------------------------------------------------
-	function RemoveBlanksBeforePHPTags ($file, $namespace) {
-		if ($namespace<>'') {
-		   $namespace = str_replace('::','\\',$namespace).'\\';
-		}
-		$fileNameFull = IPS_GetKernelDir().'scripts\\'.$namespace.$file;
-		if (!file_exists($fileNameFull)) {
-			echo 'File '.$file.' not exists (Namespace='.$namespace.')'.PHP_EOL;
-			return;
-		}
-		
-		$fileContent = file_get_contents($fileNameFull, true);
-
-		$pos = strpos($fileContent, ' ');
-		if ($pos === false or $pos > 0) {
-		   return;
-		}
-		$fileContentNew = substr($fileContent, 1);
-		echo 'Remove Blanks before PHP Tag from File '.$file.'(Namespace='.$namespace.')'.PHP_EOL;
-		file_put_contents($fileNameFull, $fileContentNew);
-	}
-
 	// ------------------------------------------------------------------------------------------------
 	function get_WateringCirclyId($DeviceName, $ParentId) {
 		$CategoryId = IPS_GetObjectIDByIdent($DeviceName, $ParentId);
