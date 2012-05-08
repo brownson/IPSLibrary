ccab31d4f6b304fc7a92271f5fc2470a3ddf3a8f
MCS-51 <a.czwalina@web.de>
2012-05-09 00:30:49 +0200

    IPSTwilight mit Mondphase

--- IPSLibrary/config/modules/Weather/IPSTwilight/Default/IPSTwilight_Custom.inc.php
+++ IPSLibrary/config/modules/Weather/IPSTwilight/Default/IPSTwilight_Custom.inc.php
@@ -14,7 +14,7 @@
 	 *
 	 * You should have received a copy of the GNU General Public License
 	 * along with the IPSLibrary. If not, see http://www.gnu.org/licenses/gpl.txt.
-	 */    
+	 */
 
 
 	/**@addtogroup ipstwilight_configuration
@@ -24,8 +24,8 @@
 	 *
 	 * Für jede Variable von IPSTwilight kann eine entsprechende Callback Funktion angelegt werden.
 	 *
-	 * Das Programm kontrolliert beim Berechnen der Dämmerungszeiten, ob eine entsprechende Funktion 
-	 * vorhanden ist und legt gegebenenfalls einen ensprechenden Timer an. 
+	 * Das Programm kontrolliert beim Berechnen der Dämmerungszeiten, ob eine entsprechende Funktion
+	 * vorhanden ist und legt gegebenenfalls einen ensprechenden Timer an.
 	 *
 	 * Folgende Callback Funktionen werden unterstützt:
 	 *  - IPSTwilight_SunriseBegin()
@@ -57,18 +57,72 @@
 	 */
 	function IPSTwilight_SunriseBegin() {
 		IPSLogger_Dbg(__file__, 'Call to customer specific Function "IPSTwilight_SunriseBegin"');
+
+		$anwesend = GetValueBoolean(35206 );
+		$scharf = GetValueBoolean(23793 );
+
+//		echo "Es müssen alle 2 Bedingungen erfüllt sein:\n";
+//		if ($anwesend == false) echo " - Keine Person anwesend\n";
+//		if ($scharf == true) echo " - Haus ist scharf\n";
+
+		if ( ($anwesend == false) and ($scharf == true))
+		{
+			HM_WriteValueFloat(26178 , "LEVEL", 100);
+			HM_WriteValueFloat(53703 , "LEVEL", 100);
+			HM_WriteValueFloat(32138 , "LEVEL", 100);
+		}
+
 	}
-	
+
 	/** Callback Methode, die beim Sonnenuntergang aufgerufen wird
 	 *
 	 */
 	function IPSTwilight_SunriseEnd() {
 		IPSLogger_Dbg(__file__, 'Call to customer specific Function "IPSTwilight_SunriseEnd"');
+
+		$anwesend = GetValueBoolean(35206 );
+		$scharf = GetValueBoolean(23793 );
+
+//		echo "Es müssen alle 2 Bedingungen erfüllt sein:\n";
+//		if ($anwesend == false) echo " - Keine Person anwesend\n";
+//		if ($scharf == true) echo " - Haus ist scharf\n";
+
+		if ( ($anwesend == false) and ($scharf == true))
+		{
+			HM_WriteValueFloat(26178 , "LEVEL", 0);
+			HM_WriteValueFloat(53703 , "LEVEL", 0);
+			HM_WriteValueFloat(32138 , "LEVEL", 0);
+		}
 	}
-	
-	 
-	 
-	 
+
+	function IPSTwilight_CivilBegin() {
+		IPSLogger_Dbg(__file__, 'Call to customer specific Function "IPSTwilight_CivilBegin"');
+	}
+
+	function IPSTwilight_CivilEnd() {
+		IPSLogger_Dbg(__file__, 'Call to customer specific Function "IPSTwilight_CivilEnd"');
+	}
+
+	function IPSTwilight_NauticBegin() {
+		IPSLogger_Dbg(__file__, 'Call to customer specific Function "IPSTwilight_NauticBegin"');
+	}
+
+	function IPSTwilight_NauticEnd() {
+		IPSLogger_Dbg(__file__, 'Call to customer specific Function "IPSTwilight_NauticEnd"');
+	}
+
+	function IPSTwilight_AstronomicBegin() {
+		IPSLogger_Dbg(__file__, 'Call to customer specific Function "IPSTwilight_AstronomicBegin"');
+	}
+
+	function IPSTwilight_AstronomicEnd() {
+		IPSLogger_Dbg(__file__, 'Call to customer specific Function "IPSTwilight_AstronomicEnd"');
+		HM_WriteValueBoolean(29668, "STATE", false);
+	}
+
+
+
+
 	/** @}*/
 
 ?>
