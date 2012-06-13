165dfee9f52356e5a72679db2358e20067e95d17
domizei385 <dominik.zeiger@gmail.com>
2012-06-14 00:28:48 +0200

    FritzBox - new configuration file

--- IPSLibrary/config/hardware/FritzBox/Default/FritzBox_Configuration.inc.php
+++ IPSLibrary/config/hardware/FritzBox/Default/FritzBox_Configuration.inc.php
@@ -1,9 +1,12 @@
 <?
+    // constants
+    define("FRITZBOX_DATA_BASE_PATH", "Program.IPSLibrary.data.hardware.FritzBox");
+    
     define("DEVICE_IP", "ip");
     define("DEVICE_PASSWORD", "pass");
 
     // ========================================================================================================================
-    // Defintion of FritzBoxes
+    // Definition of FritzBoxes
     // ========================================================================================================================
     function get_FritzBoxDevices () {
         $devices = array();
@@ -15,5 +18,30 @@
         
         return $devices;
     }
-
+    
+    // ========================================================================================================================
+    // Definition of Settings
+    // ========================================================================================================================
+    function get_FritzBoxSettings() {
+        $settings = array(
+            "LOG"                   => array(
+                "MINUTES_BETWEEN_LOGS"          => 30,
+                "dB"        => array(
+                    // log when the value has changed at least by the given amount
+                    //"MIN_CHANGE"                => 3,
+                    // log when the value has changed at least by the given amount
+                    "MIN_CHANGE_PERCENT"         => 35,
+                    // force log when the value has changed at least by the given amount
+                    "MIN_CHANGE_PERCENT_FORCE"   => 70,
+                    // force log every x minutes
+                    "MINUTES_BETWEEN_LOGS"       => 5,
+                    // log when the value has changed at least by the given amount
+                    "MINUTES_BETWEEN_LOGS_MIN_CHANGE"         => 20,
+                ),
+            ),
+        );
+        
+        return $settings;
+    }
+    
 ?>
