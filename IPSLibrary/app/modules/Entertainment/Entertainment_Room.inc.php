dfb5719fa993c079cadfac0a2e12d78cf763568c
brownson <brownson@gmx.at>
2012-12-13 20:53:07 +0100

    Entertainment - Merged Changes from Domizei

--- IPSLibrary/app/modules/Entertainment/Entertainment_Room.inc.php
+++ IPSLibrary/app/modules/Entertainment/Entertainment_Room.inc.php
@@ -79,13 +79,13 @@
 
 	// ---------------------------------------------------------------------------------------------------------------------------
 	function Entertainment_SetRoomPowerByDeviceName($DeviceName, $Value) {
-		if ($Value) {
-			$SourceList = get_SourceListByDeviceName($DeviceName, $Value);
+	   if ($Value) {
+	      $SourceList = get_SourceListByDeviceName($DeviceName);
 			if (count($SourceList)==1) {
 				$RoomKeys  = array_keys($SourceList);
 				$RoomId    = $RoomKeys[0];
-				$SourceIdx = $SourceList[$RoomId];
 				if (!IsRoomPoweredOn($RoomId)) {
+                    $SourceIdx = $SourceList[$RoomId];
 					Entertainment_SetRoomPowerByRoomId($RoomId, true, false);
 					Entertainment_SetSourceByRoomId($RoomId, $SourceIdx);
 				}
